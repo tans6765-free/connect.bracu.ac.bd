21ad87322100347e917f06fe27e081e4771210a6
Md Tahsinul Islam <md.tahsinul.islam@g.bracu.ac.bd>
2026-05-12 18:25:47 +0600

    Fix 71

--- uni_portal/settings.py
+++ uni_portal/settings.py
@@ -30,6 +30,8 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
+    'allauth.socialaccount.middleware.SocialAccountMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
